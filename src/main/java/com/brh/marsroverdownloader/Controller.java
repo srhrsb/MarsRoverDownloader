8b832ab33636a39d10b739fcc4ec84ea6e82b05a
srhrsb <sebastian.boettcher2@srh.de>
2024-11-05 11:59:59 +0100

    Stand 4UE

--- src/main/java/com/brh/marsroverdownloader/Controller.java
+++ src/main/java/com/brh/marsroverdownloader/Controller.java
@@ -3,15 +3,24 @@ package com.brh.marsroverdownloader;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
+    private Label targetFolder;
 
     @FXML
     protected void searchTargetFolder( Event ev ) {
-        System.out.println( ev.getEventType() );
-    }
+        System.out.println( ev.getTarget() );
 
+        Stage stage = (Stage) targetFolder.getScene().getWindow();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File folder = directoryChooser.showDialog( stage );
+
+        targetFolder.setText( folder.getAbsolutePath() );
+    }
 
 }
