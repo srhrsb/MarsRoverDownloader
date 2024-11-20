99b90a4d305864baaa0965ed1423f67e7704b8cc
srhrsb <sebastian.boettcher2@srh.de>
2024-11-20 15:29:55 +0100

    Stand 4UE +1

--- src/main/java/com/brh/marsroverdownloader/APIRequest.java
+++ src/main/java/com/brh/marsroverdownloader/APIRequest.java
@@ -1,4 +1,26 @@
 package com.brh.marsroverdownloader;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.function.Consumer;
+
 public class APIRequest {
+
+       private Consumer<APIResponse> onSuccessCallback;
+       private final String apiKey = "FDhYDjlm3UIpxzK5oH2XNcZui09r46v7DRn1spIY";
+
+       public void getRoverImageByDate(LocalDate date, Consumer<APIResponse> onSuccessCallback ){
+           this.onSuccessCallback = onSuccessCallback;
+
+       }
+
+       private void sendRequest( LocalDate date ){
+           String urlString ="https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="+ date.toString()+"&api_key="+apiKey;
+
+       }
+
+
+
+
+
 }
--- src/main/java/com/brh/marsroverdownloader/APIResponse.java
+++ src/main/java/com/brh/marsroverdownloader/APIResponse.java
@@ -1,4 +1,35 @@
 package com.brh.marsroverdownloader;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class APIResponse {
+
+    private LocalDate date;
+    private String imgURL;
+    private String cameraName;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    public String getCameraName() {
+        return cameraName;
+    }
+
+    public void setCameraName(String cameraName) {
+        this.cameraName = cameraName;
+    }
 }
